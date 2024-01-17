2fbef4a01f1714f58f8c21e6e4de9e5e729893a2
Mainak Jas <mainakjas@gmail.com>
2024-01-17 13:29:28 -0500

    Update webpage

--- _downloads/f13b98d66771856fc356868fa4f796ff/plot_hcp_mvcmi.py
+++ _downloads/f13b98d66771856fc356868fa4f796ff/plot_hcp_mvcmi.py
@@ -7,26 +7,28 @@ This example demonstrates how to run MVCMI on
 pre-processed HCP data.
 """
 
-# Authors: Padma Sundaram
-#          Mainak Jas
+# Authors: Padma Sundaram <padma@nmr.mgh.harvard.edu>
+#          Mainak Jas <mjas@mgh.harvard.edu>
 
 # %%
 # we will first load the necessary modules
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mvcmi import compute_cmi, compute_ccoef_pca, reduce_dim
+from mvcmi import compute_cmi, compute_ccoef_pca, generate_noise_ts, z_score
+from mvcmi.pca import reduce_dim
 from mvcmi.datasets import fetch_hcp_sample, load_label_ts
 
 from joblib import Parallel, delayed
 
-n_jobs = 1 # number of cores to use when running PCA in parallel
+n_jobs = 30 # number of cores to use when running PCA in parallel
 n_parcels = 10 # just to make example run faster
 dim_red = 0.95
 
 # %%
 # load the preprocessed data
-data_path = fetch_hcp_sample()
+path = '/autofs/space/meghnn_001/users/mjas/github_repos/mvcmi_open/examples/mvcmi_data'
+data_path = fetch_hcp_sample(path=path)
 
 label_ts_fname = data_path / 'label_ts.npz'
 label_ts = load_label_ts(label_ts_fname, n_parcels=n_parcels)
@@ -46,7 +48,7 @@ label_ts_red = Parallel(n_jobs=n_jobs, verbose=4)(delayed(reduce_dim)(
 # %%
 # do the actual CMI computation
 print("computing cmi")
-cmimtx = compute_cmi(label_ts_red)
+data_cmi = compute_cmi(label_ts_red)
 
 # %%
 # compare to correlation coefficient
@@ -55,11 +57,33 @@ corrmtx = compute_ccoef_pca(label_ts_red)
 
 # %%
 # plot the CMI matrix
-plt.imshow(cmimtx)
+plt.imshow(data_cmi)
 plt.colorbar()
 
 # %%
 # plot the correlation matrix
 plt.figure()
 plt.imshow(corrmtx)
+plt.colorbar()
+
+# %%
+# now let us compute CMI for the null distribution. Generally, the number
+# of seeds are determined empirically. For the HCP dataset, it was observed
+# that 50 seeds are sufficient to obtain stable null distribution.
+noise_ts = generate_noise_ts(label_ts, label_ts_red, min_dim, max_dim,
+                             dim_red=dim_red, seed1=0, seed2=10,
+                             n_jobs=n_jobs)
+null_cmis = list()
+for seed, this_noise_ts in enumerate(noise_ts):  # iterate over seeds
+    print(f'mvcmi for seed {seed}')
+    null_cmis.append(compute_cmi(this_noise_ts))
+
+# %%
+# finally, we z-score the CMI values (and optionally threshold)
+z_cmi = z_score(data_cmi, np.array(null_cmis))
+
+# %%
+# let us plot the z-scored CMI values
+plt.figure()
+plt.imshow(z_cmi)
 plt.colorbar()
